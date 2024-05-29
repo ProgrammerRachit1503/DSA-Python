7e526ec564e2f52e418974e60dc8d8118efd9d1d
Rachit Jain <220160212042.rachit@gdgu.org>
2024-05-30 01:29:52 +0530

    Create Subarray Sum.py

---
+++ Hash Table/Interview Questions/Subarray Sum.py
@@ -0,0 +1,48 @@
+def subarray_sum(nums : list[int], target : int) -> list[int]:
+  cur_sum : int = 0
+  Map : dict[int : int] = {}
+
+  for i in range(len(nums)):
+    cur_sum += nums[i]
+    
+    if ((cur_sum - target) == 0):
+      return [0, i]
+
+    if (cur_sum - target) in Map.keys():
+      return [Map[cur_sum - target] + 1, i]
+    
+    Map[cur_sum] = i
+  
+  return []
+
+
+def main() -> None:
+  nums = [1, 2, 3, 4, 5]
+  target = 9
+  print ( subarray_sum(nums, target) )
+
+  nums = [-1, 2, 3, -4, 5]
+  target = 0
+  print ( subarray_sum(nums, target) )
+
+  nums = [2, 3, 4, 5, 6]
+  target = 3
+  print ( subarray_sum(nums, target) )
+
+  nums = []
+  target = 0
+  print ( subarray_sum(nums, target) )
+
+if __name__ == "__main__" :
+  main()
+
+
+"""
+  EXPECTED OUTPUT:
+  ----------------
+  [1, 3]
+  [0, 3]
+  [1, 1]
+  []
+
+"""
