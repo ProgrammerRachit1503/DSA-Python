216d0efad3dd5de282c81355686f27967eeed138
Rachit Jain <220160212042.rachit@gdgu.org>
2024-05-29 20:36:37 +0530

    Create Two Sums.py

---
+++ Hash Table/Interview Questions/Two Sums.py
@@ -0,0 +1,26 @@
+def two_sum(nums : list[int], target : int) -> list[int]:
+  Map : dict[int : int] = {}
+
+  for i in range(len(nums)):
+    compliment : int = target - nums[i]
+
+    if compliment in Map.keys():
+      return [Map[compliment], i]
+    
+    Map[nums[i]] = i
+  
+  return []
+
+
+def main() -> None:
+  print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+  print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+  print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+  print(two_sum([1, 3, 5, 7, 9], 10))  
+  print (two_sum([1, 2, 3, 4, 5], 10))
+  print (two_sum([1, 2, 3, 4, 5], 7))
+  print (two_sum([1, 2, 3, 4, 5], 3))
+  print (two_sum([], 0))
+
+if __name__ == "__main__" :
+  main()
